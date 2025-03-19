d7ceed3b1589a17bb58d5365cd57c77869251ce0
amymcevoy <G00413220@atu.ie>
2025-03-19 14:04:11 +0000

    Firebase set up and .env file for APIkey

--- SnackSafe/src/environments/environment.prod.ts
+++ SnackSafe/src/environments/environment.prod.ts
@@ -1,3 +1,10 @@
 export const environment = {
-  production: true
+  production: true,
+  FIREBASE_API_KEY: '',
+  FIREBASE_AUTH_DOMAIN: '',
+  FIREBASE_PROJECT_ID: '',
+  FIREBASE_STORAGE_BUCKET: '',
+  FIREBASE_MESSAGING_SENDER_ID: '',
+  FIREBASE_APP_ID: '',
+  FIREBASE_MEASUREMENT_ID: ''
 };
