0df3c8f407bfaa6cc441da092678e3d7533863a0
amymcevoy <G00413220@atu.ie>
2025-04-02 14:44:06 +0100

    Login Page functional

--- SnackSafe/src/app/pages/login/login.page.ts
+++ SnackSafe/src/app/pages/login/login.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -8,4 +9,16 @@ import { IonicModule } from '@ionic/angular';
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss']
 })
-export class LoginPage {}
+export class LoginPage {
+    email = '';
+    password = '';
+
+    constructor(private router: Router){}
+
+    login(){
+        console.log("Login clicked: ", this.email,this.password);
+        
+        //if successful
+        this.router.navigate(['/home']);
+    }
+}
