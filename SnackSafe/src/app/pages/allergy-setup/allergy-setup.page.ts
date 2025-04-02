56e8a9413814628cdcbb730939594582bc25cc40
amymcevoy <G00413220@atu.ie>
2025-04-02 22:29:04 +0100

    Fixed Bugs

--- SnackSafe/src/app/pages/allergy-setup/allergy-setup.page.ts
+++ SnackSafe/src/app/pages/allergy-setup/allergy-setup.page.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar ,IonCheckbox,IonItem,IonList,IonLabel} from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar ,IonCheckbox,IonItem,IonList,IonLabel,IonButton} from '@ionic/angular/standalone';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
 
@@ -10,7 +10,7 @@ import { AngularFirestore } from '@angular/fire/compat/firestore';
   templateUrl: './allergy-setup.page.html',
   styleUrls: ['./allergy-setup.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar,IonCheckbox,IonItem,IonList,IonLabel,CommonModule, FormsModule]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar,IonCheckbox,IonItem,IonList,IonLabel,IonButton,CommonModule, FormsModule]
 })
 export class AllergySetupPage{
   
@@ -25,7 +25,7 @@ export class AllergySetupPage{
     'Shellfish'
   ];
   
-  selected: { [key: string]: boolean } = {};
+  selectedAllergens: { [key: string]: boolean } = {};
 
   constructor(
     private afAuth: AngularFireAuth,
@@ -33,7 +33,7 @@ export class AllergySetupPage{
   ) {}
 
   async save() {
-    const chosen = Object.keys(this.selected).filter(key => this.selected[key]);
+    const chosen = Object.keys(this.selectedAllergens).filter(key => this.selectedAllergens[key]);
     const user = await this.afAuth.currentUser;
 
     if (user) {
@@ -41,9 +41,10 @@ export class AllergySetupPage{
         .collection('users')
         .doc(user.uid)
         .set({ allergens: chosen }, { merge: true });
-        
+
     console.log('Selected allergens:', chosen);
   }
 
+  }
 }
 
