35aa93da3eb663b01431dba1e60eaf56c5623511
amymcevoy <G00413220@atu.ie>
2025-04-02 14:57:49 +0100

    Homepage functional

--- SnackSafe/src/app/pages/home/home.page.ts
+++ SnackSafe/src/app/pages/home/home.page.ts
@@ -1,20 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar,IonButton } from '@ionic/angular/standalone';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.page.html',
   styleUrls: ['./home.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule,IonButton]
 })
-export class HomePage implements OnInit {
+export class HomePage{
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private router: Router) { }
+  goToScan() {
+    this.router.navigate(['/scan']);
   }
 
+  goToAllergySetup() {
+    this.router.navigate(['/allergy-setup']);
+  }
+
+  logout() {
+    this.router.navigate(['/auth']); // or add Firebase logout later
+
+  }
 }
