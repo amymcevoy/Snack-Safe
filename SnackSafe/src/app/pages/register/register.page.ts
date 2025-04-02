23f096976cdb10f93b5dbf09e2a49f9b939f8e93
amymcevoy <G00413220@atu.ie>
2025-04-02 14:49:28 +0100

    Register functional

--- SnackSafe/src/app/pages/register/register.page.ts
+++ SnackSafe/src/app/pages/register/register.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -8,4 +9,18 @@ import { IonicModule } from '@ionic/angular';
   templateUrl: './register.page.html',
   styleUrls: ['./register.page.scss']
 })
-export class RegisterPage {}
+export class RegisterPage { email = '';
+  password = '';
+
+  constructor(private router: Router) {}
+
+  register() {
+    //remove error
+    (document.activeElement as HTMLElement)?.blur();
+
+    console.log('Register clicked:', this.email, this.password);
+    
+    //if successful
+    this.router.navigate(['/home']);
+  }
+}
