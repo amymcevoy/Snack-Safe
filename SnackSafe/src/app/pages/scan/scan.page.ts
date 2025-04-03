43654e5ce8fbb454d7a4e132f5c06fd090da3605
amymcevoy <G00413220@atu.ie>
2025-04-03 13:16:01 +0100

    Scan UI fixed

--- SnackSafe/src/app/pages/scan/scan.page.ts
+++ SnackSafe/src/app/pages/scan/scan.page.ts
@@ -1,20 +1,44 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar,IonButton } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-scan',
   templateUrl: './scan.page.html',
   styleUrls: ['./scan.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule,IonButton]
 })
-export class ScanPage implements OnInit {
 
-  constructor() { }
+export class ScanPage {
 
-  ngOnInit() {
+  scanResult: {
+    name: string;
+    code: string;
+    allergens?: string[];
+  } | null = null;
+  
+  
+  scanError: string = '';
+  
+
+  scanBarcode(){
+
+    try{
+      //Result for now
+      this.scanResult = {
+        name: 'Snickers Bar',
+        code: '12345678',
+        allergens: ['Peanuts','Dairy']
+      };
+
+      this.scanError = '';
+      
+    } catch (err){
+
+      this.scanResult = null;
+      this.scanError = 'Scan failed, try again';
+    }
   }
-
 }
