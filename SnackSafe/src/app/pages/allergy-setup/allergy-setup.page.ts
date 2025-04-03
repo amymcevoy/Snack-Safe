9c342b16747313ff1ff96e89faa55becbf6e7dbc
amymcevoy <G00413220@atu.ie>
2025-04-03 20:04:31 +0100

    Users can edit and save the allergens picked

--- SnackSafe/src/app/pages/allergy-setup/allergy-setup.page.ts
+++ SnackSafe/src/app/pages/allergy-setup/allergy-setup.page.ts
@@ -5,7 +5,7 @@ import { IonContent, IonHeader, IonTitle, IonToolbar ,IonToggle,IonItem,IonList,
 import { Router } from '@angular/router';
 import { inject } from '@angular/core';
 import { Auth, getAuth } from '@angular/fire/auth';
-import { Firestore, getFirestore, doc, setDoc } from '@angular/fire/firestore';
+import { Firestore, getFirestore, doc, setDoc, getDoc } from '@angular/fire/firestore';
 
 @Component({
   selector: 'app-allergy-setup',
@@ -29,17 +29,50 @@ export class AllergySetupPage{
   
   selectedAllergens: { [key: string]: boolean } = {};
 
-  constructor(private router: Router) {}
+  private auth = inject(Auth);
+  private firestore = inject(Firestore);
+  private router = inject(Router);
 
-  async save() {
-
-    const chosen = Object.keys(this.selectedAllergens).filter(key => this.selectedAllergens[key]);
-   
-
-    console.log('Selected allergens:', chosen);
-
-    console.log('Navigating to home...');
-    this.router.navigate(['/home'])
+  ionViewWillEnter() {
+    this.loadAllergens();
   }
 
+  async save() {
+    const user = this.auth.currentUser;
+
+    if (!user) {
+      console.error('No user logged in.');
+      return;
+    }
+    const chosen = Object.keys(this.selectedAllergens).filter(key => this.selectedAllergens[key]);
+
+  try{
+    const userDoc = doc(this.firestore, 'users', user.uid);
+    await setDoc(userDoc, { allergens: chosen }, { merge: true });
+    console.log('Saved allergens:', chosen);
+    this.router.navigate(['/home'])
+  } catch(error){
+    console.error('Failed to save allergens:', error);
+
+  }
+ }
+
+ async loadAllergens() {
+  const user = this.auth.currentUser;
+  if (!user) return;
+
+  const userDoc = doc(this.firestore, 'users', user.uid);
+  const snapshot = await getDoc(userDoc);
+
+  if (snapshot.exists()) {
+    const data = snapshot.data();
+    const savedAllergens: string[] = data['allergens'] || [];
+
+    // Reset all toggles
+    this.selectedAllergens = {};
+    for (let allergen of this.allergenList) {
+      this.selectedAllergens[allergen] = savedAllergens.includes(allergen);
+      }
+    }
+  }
 }
