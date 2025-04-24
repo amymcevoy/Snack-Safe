28c846dee6fe5e6f6414da76af86f8050d2652a2
amymcevoy <G00413220@atu.ie>
2025-04-24 11:31:55 +0100

    Save scan button and popup added

--- SnackSafe/src/app/pages/results/results.page.ts
+++ SnackSafe/src/app/pages/results/results.page.ts
@@ -4,6 +4,8 @@ import { FormsModule } from '@angular/forms';
 import { IonContent, IonHeader, IonTitle, IonToolbar,IonButton} from '@ionic/angular/standalone';
 import { Router } from '@angular/router';
 import { inject } from '@angular/core';
+import { Firestore, collection, addDoc } from '@angular/fire/firestore';
+import { Auth } from '@angular/fire/auth';
 
 @Component({
   selector: 'app-results',
@@ -17,10 +19,29 @@ export class ResultsPage {
   product: any;
 
   private router = inject(Router);
+  private firestore = inject(Firestore);
+  private auth = inject(Auth);
 
   constructor(){
     const nav = this.router.getCurrentNavigation();
     this.product = nav?.extras.state?.['product'] || history.state['product'];
+  }
+
+  async saveScan() {
+    const user = this.auth.currentUser;
+    if (!user || !this.product) return;
+
+    try {
+      const scansRef = collection(this.firestore, 'users', user.uid, 'scans');
+      await addDoc(scansRef, {
+        ...this.product,
+        savedAt: new Date()
+      });
+      alert('✅ Scan saved successfully!');
+    } catch (err) {
+      console.error('Save failed:', err);
+      alert('❌ Failed to save scan.');
+    }
   }
   
   goBack() {
