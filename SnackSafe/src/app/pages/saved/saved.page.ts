2d450a29f72dcea9274152b2563d80ec89df30c7
amymcevoy <G00413220@atu.ie>
2025-04-24 21:20:25 +0100

    Saved comments added

--- SnackSafe/src/app/pages/saved/saved.page.ts
+++ SnackSafe/src/app/pages/saved/saved.page.ts
@@ -22,6 +22,7 @@ import { doc, deleteDoc } from '@angular/fire/firestore';
 
 export class SavedPage {
 
+  // Track loading state and saved scans
   loading: boolean = true;
   previousScans: any[] = [];
 
@@ -29,6 +30,7 @@ export class SavedPage {
   private firestore = inject(Firestore);
   private route = inject(Router);
   
+  // Waits for auth and then load scans
   ngOnInit() {
     this.waitForAuth().then(user => {
       if (user) {
@@ -39,33 +41,38 @@ export class SavedPage {
     });
   }
 
+  // Waits for logged in user 
   waitForAuth(): Promise<any> {
     const auth = getAuth();
     return new Promise(resolve => {
       const unsubscribe = onAuthStateChanged(auth, user => {
-        unsubscribe(); // Stop listening after first result
+        unsubscribe(); // Stop listening once user found
         resolve(user);
       });
     });
   }
 
+  // Navigate back to home
   goHome() {
     this.route.navigate(['/home']);
   }
 
+  // Loads all saved scans from users firestore collection
   async loadScans(uid: string) {
     this.loading = true;
 
     const scansRef = collection(this.firestore, 'users', uid, 'scans');
     const snapshot = await getDocs(scansRef);
 
+    // Map document into an array and stores ID's
     this.previousScans = snapshot.docs.map(doc => ({
-      id: doc.id,           // 🔥 Needed to delete the correct document
-      ...doc.data()
+      id: doc.id,   // Needed to identify which scan to delete  
+      ...doc.data() 
     }));
     this.loading = false;
   }
 
+  // Deletes scaan by document ID
   async deleteScan(scanId: string) {
     const auth = getAuth();
     const user = auth.currentUser;
@@ -75,6 +82,8 @@ export class SavedPage {
   
     try {
       await deleteDoc(scanDocRef);
+
+      // Updates local state to remove deleted scan from list
       this.previousScans = this.previousScans.filter(scan => scan.id !== scanId);
       console.log('Scan deleted:', scanId);
     } catch (err) {
